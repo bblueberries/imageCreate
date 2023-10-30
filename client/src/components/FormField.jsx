8034b5664d7113bdfc1a1800b04b08df90700127
bblueberries <tata10433@gmail.com>
2023-10-30 17:43:03 +0700

    fix: FormField input(value)

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -17,7 +17,7 @@ const FormField = ({
           htmlFor={name}
           className="block text-sm font-medium text-gray-900"
         >
-          {name}
+          {labelName}
         </label>
         {isSurpriseMe && (
           <button
@@ -34,6 +34,7 @@ const FormField = ({
         id={name}
         name={name}
         placeholder={placeholder}
+        value={value}
         onChange={handleChange}
         required
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3 "
