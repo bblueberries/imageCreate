a03ca54553aaf8f06f8c02b4487293a4c353bc4c
bblueberries <tata10433@gmail.com>
2023-10-31 20:02:19 +0700

    fix: dalleRoute openai's version syntax mygration

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,9 +1,19 @@
 import express from "express";
 import * as dotenv from "dotenv";
-import { Configuration, OpenAiApi } from "openai";
+import OpenAI from "openai";
 
 import Post from "../mongodb/models/post.js";
 
 dotenv.config();
 
 const router = express.Router();
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY, // This is also the default, can be omitted
+});
+
+router.route("/").get((req, res) => {
+  res.send("Hello from DALL-E");
+});
+
+export default router;
