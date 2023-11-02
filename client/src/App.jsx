80373d3ff5365c69e9207df987727850c931e7eb
bblueberries <tata10433@gmail.com>
2023-11-02 14:18:37 +0700

    cleanup: change descriptions sentence&logo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
     <BrowserRouter>
       <header className=" w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
         <Link to="/">
-          <img src={logo} alt="logo" className="w-28 object-contain" />
+          <h1 className=" text-2xl font-semibold">HOME</h1>
         </Link>
         <Link
           to="/create-post"
